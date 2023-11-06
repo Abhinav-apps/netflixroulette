c2360b2abc77b14b6bc082e6777e870ae0ead8ab
Abhinav_Agrawal <aagrawal@ebsco.com>
2023-11-06 13:13:19 +0530

    Added code for Add and Edit movie

--- src/App.js
+++ src/App.js
@@ -5,13 +5,21 @@ import './index.css';
 import './components/Header/header.css';
 import MoviesList from './components/Movies/MovieList';
 import SortAndGenreControl from './components/SortAndGenreControl/SortAndGenreControl';
+import Dialog from './components/Dialog'; // Import the Dialog component
+import MovieForm from './components/MovieForm'; // Import the MovieForm component
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState(null);
   const [currentSort, setCurrentSort] = useState('releaseDate');
 
+  const [movieData, setMovieData] = useState(null); // Add state to manage movie data
+
   const handleSearch = (query) => {
     alert(`Searching for: ${query}`);
+  };
+
+  const handleMovieFormSubmit = (data) => {
+    alert(`Submiting data: ${data}`);
   };
 
   const handleGenreSelect = (genre) => {
@@ -24,21 +32,42 @@ function App() {
     alert(`Sorted By: ${sortOption}`);
   };
 
+  
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const openDialog = () => {
+    setIsDialogOpen(true);
+  };
+
+  const closeDialog = () => {
+    setIsDialogOpen(false);
+  };
+
   return (
-    <div className="div-container">
-      <Counter initialValue={10} />
-      <SearchForm initialSearchQuery="What do you want to watch?" onSearch={handleSearch} />
-      <SortAndGenreControl
-        genres={['All', 'Documentary', 'Comedy', 'Horror', 'Crime']}
-        selectedGenre={selectedGenre}
-        onSelect={handleGenreSelect}
-        currentSort={currentSort}
-        onSortChange={handleSortChange}
-      />
-      <br />
-      <MoviesList />
-      <br />
-    </div>
+    <>
+      {isDialogOpen && (
+        <Dialog title="ADD MOVIE" onClose={closeDialog}>
+          <MovieForm initialMovie={movieData} onSubmit={(data) => handleMovieFormSubmit(data)} />
+        </Dialog>
+      )}     
+     {!isDialogOpen && (
+      <div className="div-container">
+      <button onClick={openDialog}>Add Movie</button>
+     <Counter initialValue={10} />
+     <SearchForm initialSearchQuery="What do you want to watch?" onSearch={handleSearch} />
+     <SortAndGenreControl
+       genres={['All', 'Documentary', 'Comedy', 'Horror', 'Crime']}
+       selectedGenre={selectedGenre}
+       onSelect={handleGenreSelect}
+       currentSort={currentSort}
+       onSortChange={handleSortChange}
+     />
+     <br />
+     <MoviesList onMovieEdit={openDialog} /> {/* Pass the openDialog function to the MoviesList */}
+     <br />
+   </div>
+    )}
+    </>
   );
 }
 
--- src/components/Movies/MovieCard.jsx
+++ src/components/Movies/MovieCard.jsx
@@ -3,31 +3,84 @@ import PropTypes from "prop-types";
 import MovieImage from "./MovieImage";
 import MovieInfo from "./MovieInfo";
 import "./movie.css";
+import Dialog from "../Dialog"; // Import the Dialog component
+import MovieForm from "../MovieForm"; // Import the MovieForm component
 
 class MovieCard extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      showEditMenu: false,
+      isEditDialogOpen: false,
+      movieData: null,
+    };
   }
+
+  toggleEditMenu = () => {
+    this.setState((prevState) => ({
+      showEditMenu: !prevState.showEditMenu,
+    }));
+  };
+
+  openEditDialog = (movieData) => {
+    this.setState({
+      isEditDialogOpen: true,
+      showEditMenu: false,
+      movieData: movieData,
+    });
+  };
+
+  closeEditDialog = () => {
+    this.setState({
+      isEditDialogOpen: false,
+    });
+  };
+
   render() {
     return (
-      <>
-        <div className="image-container">
-          <MovieImage
-            toggleShowFilmBody={this.props.toggleShowFilmBody}
-            img={this.props.pictureURL}
-            filmTitle={this.props.name}
-            film={this.props.film}
-          />
+      <div className="movie-card" onMouseLeave={this.closeEditMenu}>
+        <div className="dots-overlay" onClick={this.toggleEditMenu}>
+          <i className="fa fa-ellipsis-v"></i>
         </div>
+        {this.state.showEditMenu && (
+          <div className="edit-menu">
+            <div
+              className="edit-option"
+              onClick={() => this.openEditDialog(this.props.film)}
+            >
+              Edit Movie
+            </div>
+            <div className="edit-option">Delete Movie</div>
+          </div>
+        )}
+        <MovieImage
+          toggleShowFilmBody={this.props.toggleShowFilmBody}
+          img={this.props.pictureURL}
+          filmTitle={this.props.name}
+          film={this.props.film}
+        />
         <MovieInfo
           description={this.props.tagline}
           name={this.props.name}
           year={this.props.year}
         />
-      </>
+        {this.state.isEditDialogOpen && (
+          <Dialog title="Edit Movie" onClose={this.closeEditDialog}>
+            <MovieForm
+              initialMovie={this.state.movieData}
+              onSubmit={(data) => {
+                // Handle editing movie data here
+                console.log("Editing movie data:", data);
+                this.closeEditDialog();
+              }}
+            />
+          </Dialog>
+        )}
+      </div>
     );
   }
 }
+
 MovieCard.propTypes = {
   genres: PropTypes.array.isRequired,
   name: PropTypes.string.isRequired,
@@ -37,4 +90,5 @@ MovieCard.propTypes = {
   overview: PropTypes.string.isRequired,
   film: PropTypes.object.isRequired,
 };
+
 export default MovieCard;

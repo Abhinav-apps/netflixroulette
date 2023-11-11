cd958afffb1a7acbae8c84a3edf90a57dd5ece45
Abhinav_Agrawal <aagrawal@ebsco.com>
2023-11-11 17:23:20 +0530

    MovieListPage and changes added

--- src/App.js
+++ src/App.js
@@ -1,70 +1,12 @@
 import React, { useState } from 'react';
-import Counter from './components/counter';
-import SearchForm from './components/searchform';
-import './index.css';
-import './components/Header/header.css';
-import MoviesList from './components/Movies/MovieList';
-import SortAndGenreControl from './components/SortAndGenreControl/SortAndGenreControl';
-import Dialog from './components/Dialog'; // Import the Dialog component
-import MovieForm from './components/MovieForm'; // Import the MovieForm component
-import 'font-awesome/css/font-awesome.min.css';
+import MovieListPage from './components/MovieListPage';
+
 
 function App() {
-  const [selectedGenre, setSelectedGenre] = useState(null);
-  const [currentSort, setCurrentSort] = useState('releaseDate');
-
-  const [movieData, setMovieData] = useState(null); // Add state to manage movie data
-
-  const handleSearch = (query) => {
-    alert(`Searching for: ${query}`);
-  };
-
-  const handleMovieFormSubmit = (data) => {
-    closeDialog();
-    alert(`Submiting data: ${data}`);
-  };
-
-  const handleGenreSelect = (genre) => {
-    setSelectedGenre(genre);
-    alert(`Selected genre: ${genre}`);
-  };
-
-  const handleSortChange = (sortOption) => {
-    setCurrentSort(sortOption);
-    alert(`Sorted By: ${sortOption}`);
-  };
-
-  
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
-
-  const openDialog = () => {
-    setIsDialogOpen(true);
-  };
-
-  const closeDialog = () => {
-    setIsDialogOpen(false);
-  };
 
   return (
-    <div className="div-container">
-      <button className="add-movie-button" onClick={openDialog}>Add Movie</button>
-      {isDialogOpen && (
-        <Dialog title="ADD MOVIE" onClose={closeDialog}>
-          <MovieForm initialMovie={movieData} onSubmit={(data) => handleMovieFormSubmit(data)} />
-        </Dialog>
-      )}     
-      <Counter initialValue={10} />
-      <SearchForm initialSearchQuery="What do you want to watch?" onSearch={handleSearch} />
-      <SortAndGenreControl
-        genres={['All', 'Documentary', 'Comedy', 'Horror', 'Crime', 'Action']}
-        selectedGenre={selectedGenre}
-        onSelect={handleGenreSelect}
-        currentSort={currentSort}
-        onSortChange={handleSortChange}
-      />
-      <br />
-      <MoviesList onMovieEdit={openDialog} /> {/* Pass the openDialog function to the MoviesList */}
-      <br />
+    <div>
+      <MovieListPage />
     </div>
   );
 }
